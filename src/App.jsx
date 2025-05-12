1ad6458046416636093d41e03b03f30377e52bea
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-12 13:26:45 +0200

    [UPD] added array, bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 const App = () => {
   return (
-    <div>
-
+    <div className="container">
+      <h1>Lista Film</h1>
     </div>
   )
 }
